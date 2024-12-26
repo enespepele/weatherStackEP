e23c28cba4db97ecf0b0f7d2749892a1dfd14158
enes pepele <enesppl2@gmail.com>
2024-12-26 20:01:21 +0300

    Dependencies is OK

--- src/main/java/com/wheatherStackNov/weatherStackNov/WeatherStackNovApplication.java
+++ src/main/java/com/wheatherStackNov/weatherStackNov/WeatherStackNovApplication.java
@@ -1,12 +1,15 @@
 package com.wheatherStackNov.weatherStackNov;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WeatherStackNovApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(WeatherStackNovApplication.class);
 	public static void main(String[] args) {
+		logger.info("App is started");
 		SpringApplication.run(WeatherStackNovApplication.class, args);
 	}
 
