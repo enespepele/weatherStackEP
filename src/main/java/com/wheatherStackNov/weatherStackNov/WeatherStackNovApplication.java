e0236c195000f9a5071c09c15294f424b2231a24
enes pepele <enesppl2@gmail.com>
2024-12-26 19:53:18 +0300

    Initial commit

---
+++ src/main/java/com/wheatherStackNov/weatherStackNov/WeatherStackNovApplication.java
@@ -0,0 +1,13 @@
+package com.wheatherStackNov.weatherStackNov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStackNovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStackNovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wheatherStackNov/weatherStackNov/WeatherStackNovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheatherStackNov.weatherStackNov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherStackNovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
